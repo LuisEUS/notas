ba0b1ea1bb14c45373bcaa0e0c323ef08e6d0805
Luis Eduardo Uscanga Serrano <isc18350503@gmail.com>
2022-04-26 10:05:23 -0500

    Se realizó la implementación de botones para el guardado de información

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,108 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
-  //TODO: Presentar el concepto del "state"
-  //hooks
- const [inputState, setInputState] = useState({
-   titulo: "", 
-   fecha: "", 
-   nota: "",
- });
-
+  //Hooks
+  const [inputState, setInputState] = useState({
+    titulo: "",
+    fecha: "",
+    nota: "",
+  });
 
   const handleInputChange = (event) => {
     setInputState({
       ...inputState,
       [event.target.name]: event.target.value,
     });
+
   };
 
-  const handleResetChange = () => {
+  const handleResetClick = () =>{
     setInputState({
       ...inputState,
-    titulo: "", 
-    fecha: "", 
-    nota: ""
-  });
-  };
+      titulo: "",
+      fecha: "",
+      nota: "",
+    });
 
+  }
+
+  const handleClickGuardar =() =>{
+    localStorage.setItem("notas", JSON.stringify(inputState));
+  }
+  
   return (
-    <div className="App">
-      <h3>Notas</h3>
-      <label htmlfor = "titulo">Titulo</label>
-      <input 
-        id = "titulo" 
-        name = "titulo" 
-        type = "text" 
-        onChange = {handleInputChange}
-        value = {inputState.titulo}
-      />
-      <br></br>
-      <label htmlfor = "fecha">Fecha</label>
-      <input 
-        id = "fecha"
-        name = "fecha" 
-        type = "text" 
-        onChange = {handleInputChange}
-        value = {inputState.fecha}
-      />
-      <br></br>
-      <label htmlfor = "nota">Nota</label>
-      <input 
-        id = "nota" 
-        name = "nota" 
-        type = "text" 
-        onChange = {handleInputChange}
-        value = {inputState.nota}
-      />
-    <button
-          type="button" 
-          className="btn btn-primary mx-2" 
-          onClick={handleResetChange}
-          style={{marginLeft:"15px"}}
-        >Reinicio
-    </button>
+    <div className="App container">
+      <div className="row">
+        <div className="col">
+          <h3>Lista</h3>
+        </div>
 
+        <div className="col">
+        <h3>Hola</h3>
+        <label className="mb-2">
+          Título 
+          <input 
+            id = "titulo" 
+            name = "titulo" 
+            type = "text" 
+            onChange = {handleInputChange}
+            value = {inputState.titulo}
+        /></label>
+        
+        <br></br>
+        <label className="mb-2">
+          Fecha
+          <input 
+            id = "fecha" 
+            name = "fecha" 
+            type = "text" 
+            onChange = {handleInputChange}
+            value = {inputState.fecha}
+        /></label>
+        
+        <br></br>
+        <label htmlfor = "nota">
+          Nota
+          <input 
+            id = "nota" 
+            name = "nota" 
+            type = "text" 
+            onChange = {handleInputChange}
+            value = {inputState.nota}
+            style = {{width: "100%"}}
+
+        /></label>
+
+      <hr></hr>
+      <div className="ms-2 me-2 mt-2 row">
+
+      <div className="col">
+        <span className="row me-1">
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={handleResetClick}
+          >
+            Limpiar
+          </button>
+          </span>
+        </div>
+
+        <div className="col">
+          <span className="row ms-1">
+          
+          <button 
+            type="button"
+            className="btn btn-secondary"
+            onClick={handleClickGuardar}>
+            Guardar
+          </button>
+          </span>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
