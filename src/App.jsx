eec9bed78e3dc8220da608771243a8152ef9e17c
Luis Eduardo Uscanga Serrano <isc18350503@gmail.com>
2022-04-27 09:50:18 -0500

    Se creó una función para almacenar varias notas

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,12 @@ function App() {
   }
 
   const handleClickGuardar =() =>{
-    localStorage.setItem("notas", JSON.stringify(inputState));
+    let arregloNotas = JSON.parse 
+    (localStorage.getItem("notas")) || []
+    arregloNotas.push(inputState);
+
+    localStorage.setItem("notas", JSON.stringify(arregloNotas));
+    handleResetClick();
   }
   
   return (
